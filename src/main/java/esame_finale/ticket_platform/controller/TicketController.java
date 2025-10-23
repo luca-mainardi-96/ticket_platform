2627f9e248a5cbd4b995afc99fddd48bca5836c6
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-10-23 10:36:31 +0200

    modifiche da ultimare

--- src/main/java/esame_finale/ticket_platform/controller/TicketController.java
+++ src/main/java/esame_finale/ticket_platform/controller/TicketController.java
@@ -3,13 +3,18 @@ package esame_finale.ticket_platform.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import esame_finale.ticket_platform.model.Ticket;
 import esame_finale.ticket_platform.repository.CategoriaRepository;
 import esame_finale.ticket_platform.repository.OperatoreRepository;
 import esame_finale.ticket_platform.repository.TicketRepository;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ticket")
@@ -30,6 +35,24 @@ public class TicketController {
         model.addAttribute("operLista", opRepo.findAll());
         model.addAttribute("catLista", catRepo.findAll());
         return "ticket/edit";
+    }
+
+    @PostMapping("/edit")
+    public String edit(@Valid @ModelAttribute("ticket") Ticket formTicket, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "ticket/edit";
+        }
+
+        ticketRepo.save(formTicket);
+        return "redirect:/ticket";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String modify(@PathVariable("id") Integer id, Model model){
+        Ticket tic = ticketRepo.findById(id).get();
+        model.addAttribute("editMode", true);
+        model.addAttribute("ticket", tic);
+        return null;
     }
 
 }
